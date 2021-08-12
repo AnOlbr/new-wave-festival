bf5cf4d8aea076b3ad6a5c598a473f011b59ca90
Aneta Olbromska <anetaolbromska@gmail.com>
2021-08-12 19:58:06 +0200

    Add endpoints

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
+const testimonialsRoutes = require('./routes/testimonials.routes');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/', concertsRoutes);
+app.use('/api/', seatsRoutes);
+app.use('/api/', testimonialsRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found...' });
+});
+
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+    console.log('Server is running on port: 8000');
 });
