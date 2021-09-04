244996c4c5aa86685d63a3b79d979c4b873ea51c
Aneta Olbromska <anetaolbromska@gmail.com>
2021-09-04 14:47:17 +0200

    Add mongoose

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const cors = require('cors');
-const path = require('path');
 const express = require('express');
+const cors = require('cors');
+const socket = require('socket.io');
+const mongoose = require('mongoose');
+
+const path = require('path');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const testimonialsRoutes = require('./routes/testimonials.routes');
-const socket = require("socket.io");
-
 const app = express();
 
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log("Server is running on port: 8000");
+  console.log('Server is running on port: 8000');
 });
 
 const io = socket(server, { cors: true });
-
-io.on("connection", (socket) => {
-  console.log("New client! Its id – " + socket.id);
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', {
+  useNewUrlParser: true,
 });
+const db = mongoose.connection;
 
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', (err) => console.log('Error ' + err));
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+});
 app.use((req, res, next) => {
   req.io = io;
   next();
@@ -25,15 +34,16 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-
-app.use('/api/', concertsRoutes);
-app.use('/api/', seatsRoutes);
-app.use('/api/', testimonialsRoutes);
 app.use(express.static(path.join(__dirname, '/client/build')));
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+app.use('/api', testimonialsRoutes);
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
+});
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found...' });
+  res.status(404).send('Not found...');
 });
